cf5dce31037c83dc76d6ff1db01be1e60899c832
rasp1415 <rasp1415@gmail.com>
2016-11-09 14:29:53 +0200

    Test un/trust respresentation. Manually masking PSR

--- leon_design/grlib-gpl-1.5.0-b4164/designs/leon3mp/systest.c
+++ leon_design/grlib-gpl-1.5.0-b4164/designs/leon3mp/systest.c
@@ -1,12 +1,33 @@
+#define TURN_TRUSTED setpsr(xgetpsr() | (0x7 << 16))
+#define TURN_UNTRUSTED setpsr(xgetpsr() & ~(0x7 << 16))
 
+
+int report_string(const char *array, unsigned int len) {
+	int i;
+	for (i = 0; i < len; ++i) {
+		report_subtest(array[i]);
+	}
+
+	return 0;
+}
+
+inline int report_int(int val) {
+	return report_string((const char*)&val, sizeof(val));
+}
 
 main()
-
 {
-
 	report_start();
 
-	base_test();
+	report_int(xgetpsr());
+	TURN_TRUSTED;
+	report_int(xgetpsr());
+	TURN_UNTRUSTED;
+	report_int(xgetpsr());
+	//report_subtest(xgetpsr());
+	//printf("psr: %d!\n", 1);
+	//base_test();
 
 	report_end();
+	return 0;
 }
